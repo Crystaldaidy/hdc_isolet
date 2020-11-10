c5c95d7e3f83ed8e0392f6125c3b8db603833bbb
Crystaldaidy <41798973+Crystaldaidy@users.noreply.github.com>
2020-11-11 02:26:31 +0800

    Update and rename isolet_data_packing.py to isolet_data_packing_64.py

--- isolet_data_packing_64.py
+++ isolet_data_packing_64.py
@@ -14,13 +14,14 @@ numClasses = 26
 quantLevels = 100
 dim = 10000
 
-hdTrainData = np.load("hdTrainData.npy")
-hdTestData = np.load("hdTestData.npy")
+in_train = np.load("pack_train.npy")
+in_test = np.load("pack_test.npy")
 
 #data-packing bitwidth
+in_bw = 64
 bw = 100
 
-def kernel(hdTrainData, trainLabels, hdTestData, testLabels, rdv3, epoch):
+def kernel(in_train, trainLabels, in_test, testLabels, rdv3, epoch):
     def learn(k, hdTrainData, prototype, prototypeCounter):
         #Find samples that have the label k
         match = hcl.compute(hdTrainData.shape, 
@@ -52,23 +53,23 @@ def kernel(hdTrainData, trainLabels, hdTestData, testLabels, rdv3, epoch):
             numb.v &= numb.v - 1
         return count.v
 
-    def test_hdc_accu(proto, hyper_dataset, labels, type):
+    def test_hdc_accu(proto, pack_data, labels, type):
         #pack the prototype
         pack_proto = hcl.pack(proto, axis=1, dtype=hcl.UInt(bw), name="pack_proto") 
 
         ###data preparation
-        distance1 = hcl.compute((hyper_dataset.shape[1],), lambda x: 0, 'distance1', dtype=hcl.UInt(bw))
-        pre_hamming = hcl.compute((hyper_dataset.shape[1],), lambda x: 0, "pre_hamming")
+        distance1 = hcl.compute((pack_data.shape[1],), lambda x: 0, 'distance1', dtype=hcl.UInt(bw))
+        pre_hamming = hcl.compute((pack_data.shape[1],), lambda x: 0, "pre_hamming")
         hamming_dist1 = hcl.compute((numClasses,), lambda x: 0, "hamming_dist1")
-        m1 = hcl.reduce_axis(0, hyper_dataset.shape[1], "m1")
+        m1 = hcl.reduce_axis(0, pack_data.shape[1], "m1")
         correct1 = hcl.scalar(0, 'correct1')
         ###
 
-        with hcl.for_(0, hyper_dataset.shape[0]) as i:
+        with hcl.for_(0, pack_data.shape[0]) as i:
             hcl.print((i),"%d suc\n")
             with hcl.for_(0, numClasses) as n:
                 #Do hdc multiplication(XOR) on sample[i]'s hdv and prototype[n]'s hdv (elementwise on the high-bit data)
-                hcl.update(distance1, lambda x: hyper_dataset[i][x] ^ pack_proto[n][x])
+                hcl.update(distance1, lambda x: pack_data[i][x] ^ pack_proto[n][x])
                 #Calculate the hamming distance of the two vectors by adding 1s
                 hcl.update(pre_hamming, lambda x: popcount(distance1[x]))
                 hcl.print((),"sum of 1s suc")
@@ -85,29 +86,29 @@ def kernel(hdTrainData, trainLabels, hdTestData, testLabels, rdv3, epoch):
                 correct1.v += 1
 
         #Print the accuracy
-        all1 = hcl.scalar(hyper_dataset.shape[0], "all1", dtype=hcl.Float(32))
+        all1 = hcl.scalar(pack_data.shape[0], "all1", dtype=hcl.Float(32))
         accuracy1 = hcl.compute((1,), lambda x: correct1.v/all1.v*100, "accuracy1" , dtype=hcl.Float(32))
         with hcl.if_(type == 1):
-            hcl.print((correct1, hyper_dataset.shape[0], accuracy1[0]), "Training accu: %d/%d (%.2f%%)\n")
+            hcl.print((correct1, pack_data.shape[0], accuracy1[0]), "Training accu: %d/%d (%.2f%%)\n")
         with hcl.else_():
-            hcl.print((correct1, hyper_dataset.shape[0], accuracy1[0]), "Testing accu: %d/%d (%.2f%%)\n")
+            hcl.print((correct1, pack_data.shape[0], accuracy1[0]), "Testing accu: %d/%d (%.2f%%)\n")
 
     def update(l, prototype, prototypeCounter, max):
         hcl.print((l+1),"%d:Use hard examples to update the prototype counters.\n")
 
         ###data preparation
-        distance = hcl.compute((pack_traindata.shape[1],), lambda x: 0, 'distance', dtype=hcl.UInt(bw))
-        pre_dist = hcl.compute((pack_traindata.shape[1],), lambda x: 0, "pre_dist")
+        distance = hcl.compute((pack_train.shape[1],), lambda x: 0, 'distance', dtype=hcl.UInt(bw))
+        pre_dist = hcl.compute((pack_train.shape[1],), lambda x: 0, "pre_dist")
         hamming_dist = hcl.compute((numClasses,), lambda x: 0, "hamming_dist")
-        m = hcl.reduce_axis(0, pack_traindata.shape[1], "m")
+        m = hcl.reduce_axis(0, pack_train.shape[1], "m")
         ###
 
-        with hcl.for_(0, pack_traindata.shape[0]) as i:
+        with hcl.for_(0, pack_train.shape[0]) as i:
             hcl.print((i),"%d suc\n")
             pack_proto = hcl.pack(prototype, axis=1, dtype=hcl.UInt(bw), name="pack_proto") 
             with hcl.for_(0, numClasses) as n:
                 #Do hdc multiplication(XOR) on sample[i]'s hdv and prototype[n]'s hdv (elementwise on the high-bit data)
-                hcl.update(distance, lambda x: pack_traindata[i][x] ^ pack_proto[n][x])
+                hcl.update(distance, lambda x: pack_train[i][x] ^ pack_proto[n][x])
                 #Calculate the hamming distance of the two vectors by adding 1s
                 hcl.update(pre_dist, lambda x: popcount(distance[x]))
                 hcl.print((),"sum of 1s suc")
@@ -143,12 +144,14 @@ def kernel(hdTrainData, trainLabels, hdTestData, testLabels, rdv3, epoch):
                         prototype[pred][m] = hcl.select(prototypeCounter[pred][m] - max[pred]/2 > 0, 1, 0)
 
         #print the accuracy
-        hcl.mutate((1,), lambda x: test_hdc_accu(prototype, pack_traindata, trainLabels, 1), 'training_update')
-        hcl.mutate((1,), lambda x: test_hdc_accu(prototype, pack_testdata, testLabels, 2), 'testing_update')
+        hcl.mutate((1,), lambda x: test_hdc_accu(prototype, pack_train, trainLabels, 1), 'training_update')
+        hcl.mutate((1,), lambda x: test_hdc_accu(prototype, pack_test, testLabels, 2), 'testing_update')
 
-    #Packing the encoded training data and testing data
-    pack_traindata = hcl.pack(hdTrainData, axis=1, dtype=hcl.UInt(bw), name="pack_traindata")
-    pack_testdata = hcl.pack(hdTestData, axis=1, dtype=hcl.UInt(bw), name="pack_testdata")
+    #Unpacking the encoded training data and testing data
+    hdTrainData = hcl.unpack(in_train, axis=1, dtype=hcl.UInt(1), name="hdTrainData")
+    hdTestData = hcl.unpack(in_test, axis=1, dtype=hcl.UInt(1), name="hdTestData")
+    pack_train = hcl.pack(hdTrainData, axis=1, dtype=hcl.UInt(bw), name="pack_train")
+    pack_test = hcl.pack(hdTestData, axis=1, dtype=hcl.UInt(bw), name="pack_test")
 
     ###learn
     hcl.print((),"Learning the prototype HDVs.\n")
@@ -156,38 +159,44 @@ def kernel(hdTrainData, trainLabels, hdTestData, testLabels, rdv3, epoch):
     prototype = hcl.compute((numClasses, dim), lambda x, y: 0, "prototype", dtype=hcl.UInt(1))
     prototypeCounter = hcl.compute((numClasses, dim), lambda x, y: 0, "prototypeCounter") #Every dimension is the sum of the targeted data
 
-    #max is the number records the added vectors, later for binary voting
+    #max is the number records the added vectors, used later for binary voting
     max = hcl.compute((numClasses, ), lambda x: 0) 
     hcl.mutate((numClasses,), lambda k: learn(k, hdTrainData, prototype, prototypeCounter),"learn")
-    
+
     #Test the accuracy after learning
-    hcl.mutate((1,), lambda x: test_hdc_accu(prototype, pack_traindata, trainLabels, 1), "test_train_accu")
-    hcl.mutate((1,), lambda x: test_hdc_accu(prototype, pack_testdata, testLabels, 2), "test_test_accu")
+    hcl.mutate((1,), lambda x: test_hdc_accu(prototype, pack_train, trainLabels, 1), "test_train_accu")
+    hcl.mutate((1,), lambda x: test_hdc_accu(prototype, pack_test, testLabels, 2), "test_test_accu")
 
     ###update
-    hcl.mutate((2,), lambda x: update(x, prototype, prototypeCounter, max),"update")
+    hcl.mutate((epoch[0],), lambda x: update(x, prototype, prototypeCounter, max),"update")
 
-hcl.init(hcl.Int())
+hcl.init()
 
-hcl_hdTrainData = hcl.placeholder((hdTrainData.shape),"hcl_trainData", dtype=hcl.UInt(1))
+hcl_in_train = hcl.placeholder((in_train.shape),"hcl_in_train", dtype=hcl.UInt(in_bw))
 hcl_trainLabels = hcl.placeholder((trainLabels.shape),"hcl_trainLabels")
-hcl_hdTestData = hcl.placeholder((hdTestData.shape),"hcl_testData", dtype=hcl.UInt(1))
+hcl_in_test = hcl.placeholder((in_test.shape),"hcl_in_test", dtype=hcl.UInt(in_bw))
 hcl_testLabels = hcl.placeholder((testLabels.shape),"hcl_testLabels")
 hcl_rdv3 = hcl.placeholder((numClasses,dim),"hcl_rdv3")
 hcl_epoch = hcl.placeholder((1,),"hcl_epoch")
 
-s = hcl.create_schedule([hcl_hdTrainData, hcl_trainLabels, hcl_hdTestData, hcl_testLabels, hcl_rdv3, hcl_epoch], kernel)
+s = hcl.create_schedule([hcl_in_train, hcl_trainLabels, hcl_in_test, hcl_testLabels, hcl_rdv3, hcl_epoch], kernel)
+
+target = hcl.platform.zc706
+# target.config(compile="vitis", mode="hw_exe")
+# target.config(compile="vivado_hls", mode="csim|csyn")
+target.config(compile="vivado_hls", mode="csyn")
+
 
 # print(hcl.lower(s))
-f = hcl.build(s)
+f = hcl.build(s, target)
 
 
 
-_hdTrainData = hcl.asarray(hdTrainData, dtype=hcl.UInt(1))
+_in_train = hcl.asarray(in_train, dtype=hcl.UInt(in_bw))
 _trainLabels = hcl.asarray(trainLabels)
-_hdTestData = hcl.asarray(hdTestData, dtype=hcl.UInt(1))
+_in_test = hcl.asarray(in_test, dtype=hcl.UInt(in_bw))
 _testLabels = hcl.asarray(testLabels)
 _rdv3 = hcl.asarray(np.random.choice([0,1], size=(numClasses, dim)))
 _epoch = hcl.asarray([30])
 
-f(_hdTrainData, _trainLabels, _hdTestData, _testLabels, _rdv3, _epoch)
+f(_in_train, _trainLabels, _in_test, _testLabels, _rdv3, _epoch)
